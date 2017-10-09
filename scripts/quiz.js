738b65e029996019d2b9624f1139adad122e1e5b
Robert Reed <robert.mc.reed@gmail.com>
2017-10-09 14:02:55 -0700

    Answer and Question constructors added. Question.addAnswer method created

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,3 +1,22 @@
 'use strict';
 
-alert('test');
+/*Answer Constructor*/
+function Answer( answerText, isCorrect ) {
+  this.answerText = answerText;
+  this.isCorrect = isCorrect;
+}
+
+/*Question Constructor*/
+function Question( questionText ) {
+  this.questionText = questionText;
+  this.answers = [];
+  this.correctAnswer = -1;
+  this.selectedAnswer = -1;
+}
+
+Question.prototype.addAnswer = function( answerText, isCorrect ) { //add a new answer to a question object
+  if( typeof( isCorrect ) === 'undefined' ) { //establish a default value of false for isCorrect
+    isCorrect = false;
+  }
+  this.answers.push( new Answer( answerText, isCorrect ) );
+};
