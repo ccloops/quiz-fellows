807226744291476dcc1dcb0ff407d8f5a4e9e463
Robert Reed <robert.mc.reed@gmail.com>
2017-10-09 15:10:57 -0700

    Question renders to screen

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -20,3 +20,28 @@ Question.prototype.addAnswer = function( answerText, isCorrect ) { //add a new a
   }
   this.answers.push( new Answer( answerText, isCorrect ) );
 };
+
+Question.prototype.renderQuestion = function() {
+  var sectionEl = document.getElementById( 'quiz' );
+  sectionEl.innerHTML = null;
+  var articleEl = document.createElement( 'article' );
+  var h2El = document.createElement( 'h2' );
+  h2El.id = 'question-text';
+  h2El.textContent = this.questionText;
+  articleEl.appendChild( h2El );
+  var olEl = document.createElement( 'ol' );
+  this.answers.forEach( function( answer, index ) {
+    var liEl = document.createElement( 'li' );
+    liEl.id = 'answer' + index;
+    liEl.textContent = answer.answerText;
+    olEl.appendChild( liEl );
+  } );
+  articleEl.appendChild( olEl );
+  sectionEl.appendChild( articleEl );
+};
+
+var myQuestion = new Question( 'Who are the best teammates?' );
+myQuestion.addAnswer( 'Catherine is the best teammate', true );
+myQuestion.addAnswer( 'Joel is the best teammate', true );
+myQuestion.addAnswer( 'Rob is the best teammate', true );
+myQuestion.addAnswer( 'None of the above' );
