4e773fc3c2d63d58319b2996a8b4bb26f552f6aa
Joel Clair <clair.joel@yahoo.com>
2017-10-10 15:00:56 -0700

    local storage complete with current user

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,9 @@
 User.form = document.getElementById('form');
 User.userNameInput = document.getElementById('username');
 User.passWordInput = document.getElementById('password');
-
 var app = {
   users: [],
+  currentUser: -1
 };
 console.log(app);
 
@@ -12,6 +12,14 @@ function User (username, password) {
   this.userName = username;
   this.passWord = password;
   app.users.push(this);
+  // localStorage.currentUser = app.users.length - 1;
+}
+
+if (localStorage.users) { // adds the localstorage.users to the app.users object
+  alert('testing retrieveApp');
+  var retrieveApp = JSON.parse(localStorage.users);
+  app.users = retrieveApp;
+
 }
 
 User.handleUserLogin = function( event ) {
@@ -22,28 +30,46 @@ User.handleUserLogin = function( event ) {
   if (app.users.length === 0) {// if no users are stored then creates a new user
 
     console.log('app.users.length = 0 true');
+    confirm('Created New User \nUsername: ' + userName + '\n Password: ' + passWord);
     new User(userName, passWord);
+    localStorage.users = JSON.stringify(app.users);
+    localStorage.currentUser = 0;
 
-  } else { // check to see if user exsists
+  } else {
     for (var i = 0; i < app.users.length; i++) {
-      if (userName === app.users[i].userName && passWord === app.users[i].passWord) {
+      if (userName === app.users[i].userName) { // check to see if user exsists
         console.log('returning user');
-        alert('welcome back');
-        break;
+        if (passWord === app.users[i].passWord) {// compare the password to the username
+          console.log('password correct');
+          alert('welcome back!');
+          app.currentUser = i;
+          localStorage.currentUser = i;
+          // localStorage.currentUser = JSON.stringify(app.currentUser);
+
+          break;
+        } else {//inncorrect password verify
+          console.log('password wrong');
+          alert('Password does not match');
+        }
+
       } else {
-        alert('new user');
+        console.log('validating user');
         var userCounter = 0;
         for (var j = 0; j < app.users.length; j++) {
           if (userName !== app.users[j].userName && passWord !== app.users[j].passWord) { //add to counter for each time the the un and pw do not match
             userCounter += 1;
           }
         }
-        console.log('counter: ' + userCounter);
-        if (userCounter === app.users.length) { //if every user was accounted for with counter = every user then create new user.
-          alert('create new user');
-          new User(userName, passWord);
-        }
       }
+    }
+    console.log('counter: ' + userCounter);
+    if (userCounter === app.users.length) { //if every user was accounted for with counter = every user, then create new user.
+      confirm('Created New User \nUsername: ' + userName + '\n Password: ' + passWord);
+      new User(userName, passWord);
+      app.currentUser = userCounter;
+      localStorage.currentUser = userCounter;
+      localStorage.users = JSON.stringify(app.users);
+      alert('Welcome!');
     }
   }
 };
