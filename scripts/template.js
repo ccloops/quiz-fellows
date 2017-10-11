9507328f9154b21065eec9268e711e82dab5dbc3
Catherine Looper <loopercat@gmail.com>
2017-10-11 16:30:48 -0700

    Functionality for removing an answer with the answer button works

--- scripts/template.js
+++ scripts/template.js
@@ -14,16 +14,28 @@ function QuestionForm() {
   QuestionForm.all.push(this);
 }
 
-function AnswerForm() {
+function AnswerForm(question) {
   this.liEl = document.createElement('li');
   this.inputLabelEl = document.createElement('label');
   this.inputEl = document.createElement('input');
-  this.radioEl = document.createElement('radio');
+  this.radioEl = document.createElement('input');
   this.radioLabelEl = document.createElement('label');
   this.removeEl = document.createElement('button');
-
+  this.questionForm = question;
+  this.removeEl.addEventListener('click', this.removeAnswer.bind(this));
   this.appendAll();
 }
+
+AnswerForm.prototype.removeAnswer = function(e) {
+  event.preventDefault();
+  var index = Number(e.target.id.replace('a', ''));
+  this.questionForm.ulEl.removeChild(this.liEl);
+  var tempAnswers = this.questionForm.answers.slice(0, index);
+  console.log(this.questionForm.answers);
+  this.questionForm.answers = tempAnswers.concat(this.questionForm.answers.slice(index + 1));
+  console.log(this.questionForm.answers);
+  this.questionForm.updateAnswerForm();
+};
 
 AnswerForm.prototype.appendAll = function() {
   this.liEl.appendChild(this.inputLabelEl);
@@ -31,14 +43,29 @@ AnswerForm.prototype.appendAll = function() {
   this.liEl.appendChild(this.radioEl);
   this.liEl.appendChild(this.radioLabelEl);
   this.liEl.appendChild(this.removeEl);
-
 };
 
 QuestionForm.prototype.addNewAnswer = function() {
   event.preventDefault();
-  var newAnswer = new AnswerForm();
+  var newAnswer = new AnswerForm(this);
   this.answers.push(newAnswer);
   this.ulEl.appendChild(newAnswer.liEl);
+  this.updateAnswerForm();
+};
+
+QuestionForm.prototype.updateAnswerForm = function() {
+  for(var i = 0; i < this.answers.length; i++) {
+    var answerForm = this.answers[i];
+    answerForm.inputLabelEl.textContent = 'Answer ' + (i + 1) + ':';
+    answerForm.inputLabelEl.setAttribute('for', 'q' + this.questionIndex + 'a' + i);
+    answerForm.inputEl.setAttribute('name', 'q' + this.questionIndex + 'a' + i);
+    answerForm.inputEl.setAttribute('type', 'text');
+    answerForm.radioEl.setAttribute('type', 'radio');
+    answerForm.radioEl.setAttribute('name', 'q' + this.questionIndex);
+    answerForm.radioLabelEl.textContent = 'Correct Answer';
+    answerForm.removeEl.textContent = 'Remove Answer';
+    answerForm.removeEl.id = 'a' + i;
+  }
 };
 
 //+++++++++++++++++++++++++++++++++++++
