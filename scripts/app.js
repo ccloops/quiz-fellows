a8e5060d2eb2dfcfa85a37d6017f6a144a3317ce
Joel Clair <clair.joel@yahoo.com>
2017-10-11 14:24:36 -0700

    html elements now render in js

--- scripts/app.js
+++ scripts/app.js
@@ -32,9 +32,71 @@ User.logoutButtRender = function() { //clears login form and adds logout button.
   User.liEl.appendChild(User.buttEl);
 };
 
-if (app.currentUser > -1) {//if currentUser exsists then remove login form.
-  logoutButtRender();
-}
+User.splashPageRender = function() {
+  User.divEl = document.createElement('div');
+
+  User.h2El1 = document.createElement('h2');
+  User.h2El1.id = 'jh2';
+  User.h2El1.textContent = 'Take a Quiz';
+
+  User.h2El2 = document.createElement('h2');
+  User.h2El2.id = 'jh2';
+  User.h2El2.textContent = 'Make a Quiz';
+
+  User.h2El3 = document.createElement('h2');
+  User.h2El3.id = 'jh2';
+  User.h2El3.textContent = 'About us';
+
+  User.ul2El = document.createElement('ul');
+
+  User.liEl2 = document.createElement('li');
+  User.liEl2.id = 'jli';
+
+  User.liEl3 = document.createElement('li');
+  User.liEl3.id = 'jli';
+
+  User.liEl4 = document.createElement('li');
+  User.liEl4.id = 'jli';
+
+  User.imgEl1 = document.createElement('img');
+  User.imgEl1.id = 'jimgs';
+  User.imgEl1.src = 'stockimgs/red-quiz-button.jpg';
+
+  User.imgEl2 = document.createElement('img');
+  User.imgEl2.id = 'jimgs';
+  User.imgEl2.src = 'stockimgs/bigstock-Quill-pen-and-ink-well-resting-89718662.jpg';
+
+  User.imgEl3 = document.createElement('img');
+  User.imgEl3.id = 'jimgs';
+  User.imgEl3.src = 'stockimgs/about-us.png';
+
+  User.aEl1 = document.createElement('a');
+  User.aEl1.href = 'html/quiz.html';
+
+  User.aEl2 = document.createElement('a');
+  User.aEl2.href = 'html/template.html';
+
+  User.aEl3 = document.createElement('a');
+  User.aEl3.href = 'html/about.html';
+
+  User.main.appendChild(User.divEl);
+  User.divEl.appendChild(User.h2El1);
+  User.divEl.appendChild(User.h2El2);
+  User.divEl.appendChild(User.h2El3);
+  User.divEl.appendChild(User.ul2El);
+  User.ul2El.appendChild(User.liEl2);
+  User.ul2El.appendChild(User.liEl3);
+  User.ul2El.appendChild(User.liEl4);
+  User.liEl2.appendChild(User.aEl1);
+  User.aEl1.appendChild(User.imgEl1);
+
+  User.liEl3.appendChild(User.aEl2);
+  User.aEl2.appendChild(User.imgEl2);
+
+  User.liEl4.appendChild(User.aEl3);
+  User.aEl3.appendChild(User.imgEl3);
+};
+User.splashPageRender();
 
 if (localStorage.users) { // adds the localstorage.users to the app.users object.
   alert('testing retrieveApp');
@@ -42,6 +104,10 @@ if (localStorage.users) { // adds the localstorage.users to the app.users object
   app.users = retrieveApp;
   var retrieveCurrentUser = JSON.parse(localStorage.currentUser);
   app.currentUser = retrieveCurrentUser;
+}
+
+if (app.currentUser > -1) {//if currentUser exsists then remove login form.
+  User.logoutButtRender();
 }
 console.log(app.currentUser + 'currentuser after');
 
